d566716baf14464228de58b558bf840bbfe1bcf0
Stinkfist <chancedj@yahoo.com>
2017-08-07 20:17:41 -0400

    More updates.  changed to version 0.3.  Split helper functions to Helper.lua and added "namespaces" before the functions.  Impelemented most of the data for a player - now to make sure I can get this to work for 2 players on seperate realms (to make sure the saved variables are working).

---
+++ Helper.lua
@@ -0,0 +1,38 @@
+local _, addonHelpers = ...;
+
+function addonHelpers:fif(condition, if_true, if_false)
+  if condition then return if_true else return if_false end
+end
+
+function addonHelpers:convertDifficulty(difficulty)
+	local difficultyName = "unk: " .. difficulty
+
+	if difficulty == 14 then		difficultyName = "normal";
+	elseif difficulty == 15 then	difficultyName = "heroic";
+	elseif difficulty == 16 then	difficultyName = "mythic";
+	elseif difficulty == 17 then	difficultyName = "lfr";
+	elseif difficulty == 23 then	difficultyName = "mythic";
+	end
+
+	return difficultyName
+end
+
+-- recursive printing for debug purposes
+function addonHelpers:printTable( tbl, indent )
+	if ( tbl == nil ) then return; end;
+	
+	for key, value in next, tbl do
+		if ( type ( value ) == "table" ) then
+			print( indent .. key );
+
+			addonHelpers:printTable( value, "  " .. indent );
+		elseif( type( value ) == "boolean" ) then
+			print( indent .. key .. " - " .. addonHelpers:fif( value, "true", "false" ) );
+		else
+			print( indent .. key .. " - " .. value );
+		end;
+	end;
+	
+end
+
+return addonHelpers;
--- LockHelper.lua
+++ LockHelper.lua
@@ -1,17 +1,51 @@
+local _, addonHelpers = ...;
+
+--[[
 local function fif(condition, if_true, if_false)
   if condition then return if_true else return if_false end
 end
 
 local function convertDifficulty(difficulty)
-	local difficultyName = "unknown"
+	local difficultyName = "unk: " .. difficulty
 
 	if difficulty == 14 then		difficultyName = "normal";
 	elseif difficulty == 15 then	difficultyName = "heroic";
 	elseif difficulty == 16 then	difficultyName = "mythic";
 	elseif difficulty == 17 then	difficultyName = "lfr";
+	elseif difficulty == 23 then	difficultyName = "mythic";
 	end
 
 	return difficultyName
+end
+
+-- recursive printing for debug purposes
+local function printTable( tbl, indent )
+	if ( tbl == nil ) then return; end;
+	
+	for key, value in next, tbl do
+		if ( type ( value ) == "table" ) then
+			print( indent .. key );
+
+			printTable( value, "  " .. indent );
+		elseif( type( value ) == "boolean" ) then
+			print( indent .. key .. " - " .. fif( value, "true", "false" ) );
+		else
+			print( indent .. key .. " - " .. value );
+		end;
+	end;
+	
+end
+--]]
+local function getDeadBosses( data )
+	local deadCount = 0;
+	
+	for _, data in next, data do
+		if ( data.isKilled ) then
+			deadCount = deadCount + 1;
+		end;
+	end
+	
+	return deadCount;
 end
 
 --[[
@@ -31,30 +65,34 @@ function LockHelper_PrintMsg()
 
 	local playerName = UnitName("player");						-- get the name of the current player
 	local realmName = GetRealmName();							-- get the name of the current realm
-
-	local t = t or {};											-- initialize variable if not already initialized
+	local playerData = {};
 	
-	t[realmName] = t[realmName] or {};							-- initialize realm if not already initialized
-	t[realmName][playerName] = t[realmName][playerName] or {};	-- initialize player if not already initialized
-
 	---[[
 	print('just WoD RF Dungeons[begin]');
 	local lfrCount = GetNumRFDungeons();
-	for lfrId=1,lfrCount do
+	for lfrNdx = 1, lfrCount do
 		local instanceID, name, typeID, subtypeID
 			, minLevel, maxLevel, recLevel, minRecLevel, maxRecLevel, expansionLevel
 			, groupID, textureFilename, difficulty, maxPlayers, description, isHoliday
-			, bonusRepAmount, minPlayers, isTimeWalker, name2, minGearLevel = GetRFDungeonInfo(lfrId);
+			, bonusRepAmount, minPlayers, isTimeWalker, instanceName, minGearLevel = GetRFDungeonInfo( lfrNdx );
 
-		t[realmName][playerName][name2] = t[realmName][playerName][name2] or {};
-		
-		local numEncounters, _ = GetLFGDungeonNumEncounters(instanceID);
+		local bossData = {};
+		local numEncounters, _ = GetLFGDungeonNumEncounters( instanceID );
 
-		for ndx=1, numEncounters do
-			local bossName, _, isKilled = GetLFGDungeonEncounterInfo(instanceID, ndx)
-			if (isKilled) then
-				print('   ' .. bossName .. ' Dead' );
-			end
+		for encounterNdx = 1, numEncounters do
+			local bossName, _, isKilled = GetLFGDungeonEncounterInfo( instanceID, encounterNdx );
+			
+			bossData[ encounterNdx ] = {};
+			bossData[ encounterNdx ].bossName = bossName;
+			bossData[ encounterNdx ].isKilled = isKilled;
+		end
+
+		-- only save if we've killed a boss
+		if getDeadBosses( bossData ) > 0 then
+			local difficultyName = addonHelpers:convertDifficulty( difficulty );
+			playerData[ instanceName ] = playerData[ instanceName ] or {};
+			playerData[ instanceName ][ difficultyName ] = playerData[ instanceName ][ difficultyName ] or {};
+			playerData[ instanceName ][ difficultyName ].bossData = bossData;
 		end
 	end -- lfrId=1,lfrCount do
 	print('just WoD RF Dungeons[end]');
@@ -63,20 +101,37 @@ function LockHelper_PrintMsg()
 	---[[
 	print('show saved instances[begin]');
 	local lockCount = GetNumSavedInstances();
-	print('lockCount: ' .. lockCount);
-	for lockId=1, lockCount do
-		local name, id, reset, difficulty, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName, numEncounters, encounterProgress = GetSavedInstanceInfo(lockId);
+	for lockId = 1, lockCount do
+		local instanceName, id, reset, difficulty, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName, numEncounters, encounterProgress = GetSavedInstanceInfo( lockId );
 
-		print(name .. ' - ' .. difficultyName);
-		
-		for bossNdx=1,numEncounters do
-			local bossName, _, isKilled, _ = GetSavedInstanceEncounterInfo( lockId, bossNdx );
-			print('   ' .. bossName .. fif( isKilled, ' Dead', ' Alive'));
+		local bossData = {};
+		for encounterNdx = 1,numEncounters do
+			local bossName, _, isKilled, _ = GetSavedInstanceEncounterInfo( lockId, encounterNdx );
+			
+			bossData[ encounterNdx ] = {};
+			bossData[ encounterNdx ].bossName = bossName;
+			bossData[ encounterNdx ].isKilled = isKilled;
+		end
+
+		-- only save if we've killed a boss
+		if getDeadBosses( bossData ) > 0 then
+			local difficultyName = addonHelpers:convertDifficulty( difficulty );
+			playerData[ instanceName ] = playerData[ instanceName ] or {};
+			playerData[ instanceName ][ difficultyName ] = playerData[ instanceName ][ difficultyName ] or {};
+			playerData[ instanceName ][ difficultyName ].bossData = bossData;
 		end
 	end -- for lockId=1, lockCount do
 	print('show saved instances[end]');
 	--]]
 	print('done');
+	
+	local t = t or {};											-- initialize variable if not already initialized
+	
+	t[ realmName ] = t[ realmName ] or {};							-- initialize realm if not already initialized
+	t[ realmName ][ playerName ] = playerData;	-- initialize player if not already initialized
+	
+	addonHelpers:printTable(t, "=>");
+
 end -- end PrintMsg
 
 LockHelper_PrintMsg();
