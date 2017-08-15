46d14b9a55942eda26937d9b31e3d9fc679d70fb
chancedj <chancedj@yahoo.com>
2017-08-15 01:20:21 -0400

    get a default tooltip displaying.  ready to work in wrapping up the data in the ui now

--- Minimap.lua
+++ Minimap.lua
@@ -9,9 +9,9 @@ local LockHelperMo = LibStub("LibDataBroker-1.1"):NewDataObject("Lock Helper", {
 	type = "data source",
 	text = "Lock Helper",
 	icon = "Interface\\Icons\\Inv_misc_key_10",
-	OnClick = function() addon:OnClick() end,
-	OnEnter = function() addon:OnEnter() end,
-	OnLeave = function() addon:OnLeave() end,
+	OnClick = function( self ) addon:OnClick( self ) end,
+	OnEnter = function( self ) addon:OnEnter( self ) end,
+	OnLeave = function( self ) addon:OnLeave( self ) end,
 }); -- local LockHelperMo
 
 function addon:OnInitialize()
@@ -20,14 +20,33 @@ function addon:OnInitialize()
 	icon:Register(addonName, LockHelperMo, LockHelperMapDb.profile.minimap)
 end -- addon:OnInitialize
 
+-- Get a reference to the lib
+local LibQTip = LibStub('LibQTip-1.0')
+
 function addon:OnClick()
 	LockHelper_PrintMsg();
 end -- addon:OnClick
 
-function addon:OnEnter()
-	print("you entered ..." .. addonName);
+function addon:OnEnter( self )
+	-- Acquire a tooltip with 3 columns, respectively aligned to left, center and right
+	local tooltip = LibQTip:Acquire("LockHelperTooltip", 3, "LEFT", "CENTER", "RIGHT");
+	self.tooltip = tooltip;
+
+	-- Add an header filling only the first two columns
+	tooltip:AddHeader('Anchor', 'Tooltip');
+
+	-- Add an new line, using all columns
+	tooltip:AddLine('Hello', 'World', '!');
+
+	-- Use smart anchoring code to anchor the tooltip to our frame
+	tooltip:SmartAnchorTo(self);
+
+	-- Show it, et voilà !
+	tooltip:Show();
 end --  addon:OnEnter
 
-function addon:OnLeave()
-	print("you left ..." .. addonName);
+function addon:OnLeave( self )
+   -- Release the tooltip
+   LibQTip:Release( self.tooltip );
+   self.tooltip = nil;
 end -- addon:OnLeave
