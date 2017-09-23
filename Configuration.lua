e9470f54906d5a1db47476f1b356ea97bacb5185
Stinkfist <chancedj@yahoo.com>
2017-09-23 00:24:11 -0400

    Add config options
    Add the new embeds for the configuration options.

---
+++ Configuration.lua
@@ -0,0 +1,8 @@
+--[[
+	This file is to deal with the code to generate the lockout table/vector and
+	to handle the refresh of data and deletion of stale data
+--]]
+local addonName, addonHelpers = ...;
+
+-- libraries
+local L = LibStub( "AceLocale-3.0" ):GetLocale( addonName, false );
