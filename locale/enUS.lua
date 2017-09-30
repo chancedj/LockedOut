d13037ab347cd4fd3564abbfdad883579ce67f73
Stinkfist <chancedj@yahoo.com>
2017-09-29 23:34:08 -0400

    more tabs vs spaces fixing.

--- locale/enUS.lua
+++ locale/enUS.lua
@@ -1,31 +1,31 @@
 local addonName, _ = ...;
 
-local L = LibStub( "AceLocale-3.0" ):NewLocale( addonName, "enUS", true ); 
+local L = LibStub( "AceLocale-3.0" ):NewLocale( addonName, "enUS", true );
 
 -- addon name
-L["Locked Out"]	= "Locked Out";
+L["Locked Out"] = "Locked Out";
 
 -- tooltip labels and headers
-L["Realm"]				= "Realm";
-L["Character"]			= "Character";
-L["Dungeon"]			= "Dungeon";
-L["Raid"]				= "Raid";
-L["World Boss"]			= "World Boss";
-L["Currency"]			= "Currency";
-L["Character iLevels"]	= "Character iLevels";
-L["Defeated"]			= "Defeated";
-L["Available"]			= "Available";
+L["Realm"]              = "Realm";
+L["Character"]          = "Character";
+L["Dungeon"]            = "Dungeon";
+L["Raid"]               = "Raid";
+L["World Boss"]         = "World Boss";
+L["Currency"]           = "Currency";
+L["Character iLevels"]  = "Character iLevels";
+L["Defeated"]           = "Defeated";
+L["Available"]          = "Available";
 
 -- difficulty mapping full
-L["Unknown"]	= "Unknown";
-L["Normal"] 	= "Normal";
-L["Heroic"] 	= "Heroic";
-L["Mythic"]		= "Mythic";
-L["Lfr"]		= "Lfr";
+L["Unknown"]    = "Unknown";
+L["Normal"]     = "Normal";
+L["Heroic"]     = "Heroic";
+L["Mythic"]     = "Mythic";
+L["Lfr"]        = "Lfr";
 
 -- difficulty mapping abbreviation
-L["U"]	= "U";
+L["U"]  = "U";
 L["N"] 	= "N";
 L["H"] 	= "H";
-L["M"]	= "M";
-L["L"]	= "L";
+L["M"]  = "M";
+L["L"]  = "L";
